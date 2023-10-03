ee9eea774fd3426baf87525bd9162d0537ff9770
Irfan Hidayat Fachrurrozi <fachrurrozi.irfan@gmail.com>
2023-10-03 21:43:47 +0700

    Initial commit

---
+++ resources/views/example.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.template')
+
+@section('content')
+    <b>Hello World!</b>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi fuga vero nulla eaque maxime ipsa id cumque totam molestiae neque quam, quisquam iusto molestias quo a consequatur excepturi reiciendis quos?</p>
+@endsection
